077910fde563ed4c2b5c043b234abb312c96afb3
Paul <phapp1988@gmail.com>
2020-09-30 15:22:02 -0500

    Refactor SignupForm component to use hooks

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,7 +1,7 @@
 import Button from '@material-ui/core/Button';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -26,84 +26,79 @@ const styles = (theme) => ({
   },
 });
 
-class SignupForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      errMsg: '',
-      name: '',
-      password: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+const SignupForm = ({ classes }) => {
+  const [email, setEmail] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleChange(event) {
-    const { id, value } = event.target;
-    if (id === 'email') {
-      this.setState({ errMsg: '', email: value });
-    }
-    this.setState({ [id]: value });
-  }
+  const handleEmailFieldChange = (event) => {
+    setErrMsg('');
+    setEmail(event.target.value);
+  };
 
-  render() {
-    const { classes } = this.props;
-    const { email, errMsg, name, password } = this.state;
-    return (
-      <form
-        action="/signup"
-        autoComplete="off"
-        className={classes.form}
-        method="post"
-      >
-        <Typography className={classes.heading} variant="h4">
-          Sign Up
-        </Typography>
-        <TextField
-          fullWidth
-          id="name"
-          label="Name"
-          name="name"
-          onChange={this.handleChange}
-          required
-          value={name}
-        />
-        <TextField
-          fullWidth
-          id="email"
-          error={errMsg !== ''}
-          helperText={errMsg}
-          label="Email"
-          name="email"
-          onChange={this.handleChange}
-          required
-          value={email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          label="Password"
-          name="password"
-          onChange={this.handleChange}
-          required
-          type="password"
-          value={password}
-        />
-        <Button className={classes.button} type="submit" variant="contained">
-          Sign Up
+  const handleNameFieldChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePasswordFieldChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <form
+      action="/signup"
+      autoComplete="off"
+      className={classes.form}
+      method="post"
+    >
+      <Typography className={classes.heading} variant="h4">
+        Sign Up
+      </Typography>
+      <TextField
+        fullWidth
+        id="name"
+        label="Name"
+        name="name"
+        onChange={handleNameFieldChange}
+        required
+        value={name}
+      />
+      <TextField
+        fullWidth
+        id="email"
+        error={errMsg !== ''}
+        helperText={errMsg}
+        label="Email"
+        name="email"
+        onChange={handleEmailFieldChange}
+        required
+        value={email}
+      />
+      <TextField
+        fullWidth
+        id="password"
+        label="Password"
+        name="password"
+        onChange={handlePasswordFieldChange}
+        required
+        type="password"
+        value={password}
+      />
+      <Button className={classes.button} type="submit" variant="contained">
+        Sign Up
+      </Button>
+      <Link href="/login">
+        <Button
+          className={`${classes.button} ${classes.linkBtn}`}
+          variant="contained"
+        >
+          Login
         </Button>
-        <Link href="/login">
-          <Button
-            className={`${classes.button} ${classes.linkBtn}`}
-            variant="contained"
-          >
-            Login
-          </Button>
-        </Link>
-      </form>
-    );
-  }
-}
+      </Link>
+    </form>
+  );
+};
 
 SignupForm.propTypes = {
   classes: PropTypes.shape({
